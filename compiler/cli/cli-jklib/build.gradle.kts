673b724d74c96a18df4512c0566771be5778ad0f
Jose Enrique Estremadoyro Fort <joseefort@google.com>
2026-03-09 20:18:28 -0600

    Simplified dependencies

--- compiler/cli/cli-jklib/build.gradle.kts
+++ compiler/cli/cli-jklib/build.gradle.kts
@@ -3,15 +3,11 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":compiler:cli-base"))
     implementation(project(":compiler:cli"))
-    implementation(project(":compiler:ir.backend.common"))
     implementation(project(":compiler:ir.serialization.jklib"))
-    implementation(project(":compiler:backend.jvm"))
     implementation(project(":compiler:backend.jvm.entrypoint"))
     implementation(project(":compiler:fir:fir2ir:jvm-backend"))
     implementation(project(":compiler:cli-jvm"))
-    implementation(project(":native:native.config"))
 
     compileOnly(intellijCore())
 }
